2f7e776211d801345329611e7b7d5297c4ab17c9
Sebastian <u18001859@tuks.co.za>
2019-08-05 14:25:38 +0200

    Created Some Views

--- INF272Group11Project/Controllers/VoterController.cs
+++ INF272Group11Project/Controllers/VoterController.cs
@@ -9,7 +9,38 @@ namespace INF272Group11Project.Controllers
     public class VoterController : Controller
     {
         // GET: Voter
+        //The Index ActionResult is the main homepage for the project.
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FindVotingStation()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterVoter()
+        {
+            return View();
+        }
+
+        public ActionResult VoterLogin()
+        {
+            return View();
+        }
+
+        public ActionResult VoterHomePage()
+        {
+            return View();
+        }
+
+        public ActionResult UpdateVoterInfo()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePasswordVoter()
         {
             return View();
         }
