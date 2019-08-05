aadd103c7eaa91309ce30c7d0376ad73d457df1e
Sebastian <u18001859@tuks.co.za>
2019-08-05 12:47:30 +0200

    Add project files.

---
+++ INF272Group11Project/Controllers/VoterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Group11Project.Controllers
+{
+    public class VoterController : Controller
+    {
+        // GET: Voter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ INF272Group11Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="INF272Group11Project.MvcApplication" Language="C#" %>
