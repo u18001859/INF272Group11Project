cc4224d75d6b02c986754839c752967f7489d8f5
user <user@DELL-DESKTOP>
2019-08-06 21:51:49 +0200

    Created Candidate Controller

---
+++ INF272Group11Project/Controllers/CandidateController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Group11Project.Controllers
+{
+    public class CandidateController : Controller
+    {
+        // GET: Candidate
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
