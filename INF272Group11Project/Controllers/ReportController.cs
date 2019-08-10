6283c9995cf761b8deab9f5e9e7d2651f984d45d
Sebastian <u18001859@tuks.co.za>
2019-08-10 18:21:04 +0200

    Finished The Staff Section and made  the Report Section

---
+++ INF272Group11Project/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Group11Project.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProvincialReport()
+        {
+            return View();
+        }
+
+        public ActionResult MakeProvincialReport()
+        {
+            return View();
+        }
+
+        public ActionResult NationalReport()
+        {
+            return View();
+        }
+
+        public ActionResult MakeNationalReport()
+        {
+            return View();
+        }
+    }
+}
