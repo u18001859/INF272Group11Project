292f53ef5cf80314457d5157c19b2404cadb66f8
Sebastian <u18001859@tuks.co.za>
2019-09-26 17:57:55 +0200

    Made the voter Register and Login Funtionality

--- INF272Group11Project/Controllers/VoterController.cs
+++ INF272Group11Project/Controllers/VoterController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using INF272Group11Project.Models;
+using INF272Group11Project.ViewModels;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace INF272Group11Project.Controllers
 {
+    
     public class VoterController : Controller
     {
+        VotingSystemProjectEntities db = new VotingSystemProjectEntities();
         // GET: Voter
         //The Index ActionResult is the main homepage for the project.
         public ActionResult Index()
@@ -22,22 +28,109 @@ namespace INF272Group11Project.Controllers
 
         public ActionResult RegisterVoter()
         {
+
+            ViewBag.ProvinceID = new SelectList(db.Provinces, "ProvinceID", "ProvinceName");
+            ViewBag.CityOrTownID = new SelectList(db.CityOrTowns, "CityOrTownID", "CityOrTownName");
+            ViewBag.SuburbID = new SelectList(db.Suburbs, "SuburbID", "SuburbName");
+            ViewBag.SecurityQuestionID = new SelectList(db.SecurityQuestions, "SecurityQuestionID", "SecurityQuestion1");
+            
             return View();
         }
 
-        public ActionResult AddNewVoter()
+        [HttpPost]
+        public ActionResult AddNewVoter(string VoterIDNumber, string FirstName, string LastName, string Email, string PhoneNumber, [Bind(Include = "SuburbID, SecurityQuestionID")] Voter voter, string Address, string Password, string ConfirmPassword, string SecurityAnswer)
         {
+            string EncrypVoterID = HashedData(VoterIDNumber);
+            var searchID = db.Voters.Where(x => x.VoterIDNumber == EncrypVoterID).FirstOrDefault();
+
+            if (searchID == null)
+            {
+                var SearchEmail = db.Voters.Where(j => j.VoterEmail == Email).FirstOrDefault();
+                if(SearchEmail == null)
+                {
+                    var SearchPhone = db.Voters.Where(g => g.VoterPhoneNumber == PhoneNumber).FirstOrDefault();
+                    if(SearchPhone == null)
+                    {
+                        string EncrypPassword1 = HashedData(Password);
+                        string EncrypPassword2 = HashedData(ConfirmPassword);
+                        if (EncrypPassword1 == EncrypPassword2)
+                        {
+                            string EncrypSecurityQuestionAnswer = HashedData(SecurityAnswer);
+                            Voter v = new Voter();
+                            v.VoterIDNumber = EncrypVoterID;
+                            v.VoterPassword = EncrypPassword1;
+                            v.VoterFirstNames = FirstName;
+                            v.VoterLastName = LastName;
+                            v.VoterEmail = Email;
+                            v.VoterPhoneNumber = PhoneNumber;
+                            v.VoterStreetAddress = Address;
+                            v.VotingStatus = false;
+                            v.SecurityQuestionID = voter.SecurityQuestionID;
+                            v.SuburbID = voter.SuburbID;
+                            v.SecurityQuestionAnswer = EncrypSecurityQuestionAnswer;
+                            db.Voters.Add(v);
+                            db.SaveChanges();
+                            return RedirectToAction("VoterLogin");
+                        }
+                        else
+                        {
+                            RedirectToAction("RegisterVoter");
+                        }
+                    }
+                    else
+                    {
+                        RedirectToAction("RegisterVoter");
+                    }
+                }
+                else
+                {
+                    RedirectToAction("RegisterVoter");
+                }
+            }
+            else
+            {
+                RedirectToAction("RegisterVoter");
+            }
             return RedirectToAction("VoterLogin");
         }
 
+        string HashedData(string placeholder)
+        {
+            using (SHA256 h = SHA256.Create())
+            {
+                byte[] b = h.ComputeHash(Encoding.UTF8.GetBytes(placeholder));
+
+                StringBuilder builder = new StringBuilder();
+                for(int i=0; i< b.Length; i++)
+                {
+                    builder.Append(b[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        
         public ActionResult VoterLogin()
         {
             return View();
         }
 
-        public ActionResult LogVoterIn()
+        [HttpPost]
+        public ActionResult LogVoterIn(string IDNumber, string Password)
         {
-            return RedirectToAction("VoterHomePage");
+            string IDNum = HashedData(IDNumber);
+            string Pass = HashedData(Password);
+
+            Voter u = db.Voters.Where(x => x.VoterIDNumber == IDNum && x.VoterPassword == Pass).FirstOrDefault();
+            if(u != null)
+            {
+                return RedirectToAction("VoterHomePage");
+            }
+            else
+            {
+                return RedirectToAction("VoterLogin");
+            }
+            
         }
 
         public ActionResult VoterHomePage()
---
+++ INF272Group11Project/ViewModels/RegisterVoterVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using INF272Group11Project.Models;
+
+namespace INF272Group11Project.ViewModels
+{
+    public class RegisterVoterVM
+    {
+        public List<Province> Provinces;
+        public List<CityOrTown> CityOrTowns;
+        public List<Suburb> suburbs;
+        public List<SecurityQuestion> SecurityQuestions;
+    }
+}
