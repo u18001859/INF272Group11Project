17c7080f5f0e38aa760497fd26a93f5a2a55c2f5
user <user@DELL-DESKTOP>
2019-09-30 23:21:49 +0200

    Added Candidate error handling, dropdown lists and database input

--- INF272Group11Project/Controllers/CandidateController.cs
+++ INF272Group11Project/Controllers/CandidateController.cs
@@ -1,71 +1,201 @@
-﻿using System;
+﻿using INF272Group11Project.Models;
+using INF272Group11Project.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Cryptography;
 
 namespace INF272Group11Project.Controllers
 {
     public class CandidateController : Controller
     {
+        VotingSystemProjectEntities db = new VotingSystemProjectEntities();
+
         // GET: Candidate
 
-       public ActionResult RegisterCandidate()
+        public ActionResult RegisterCandidate(string Party, int? errorID)
         {
-            
+            if (errorID == 1)
+            {
+                ViewBag.Error1 = "Please enter all first names";
+            }
+            else if (errorID == 2)
+            {
+                ViewBag.Error2 = "Please enter a last name";
+            }
+            else if (errorID == 3)
+            {
+                ViewBag.Error3 = "Please select a party";
+            }
+            else if (errorID == 4)
+            {
+                ViewBag.Error4 = "Please select a candidate position";
+            }
             return View();
         }
 
-        public ActionResult AddNewCandidate()
+        [HttpPost]
+        public ActionResult AddNewCandidate(string FirstNames, string LastName, string Party, string Position, string submitButton, CandidateVM model)
         {
-                    return RedirectToAction("StaffHomePage", "Staff");  
+            switch (submitButton)
+            {
+                case "Register":
+                    //Validation: Prevents candidate from being registered if fields are left blank
+                    if (String.IsNullOrEmpty(FirstNames))
+                    {
+                        return RedirectToAction("RegisterCandidate", "Candidate", new { @errorID = 1 });
+                    }
+                    if (String.IsNullOrEmpty(LastName))
+                    {
+                        return RedirectToAction("RegisterCandidate", "Candidate", new { @errorID = 2 });
+                    }
+                    if (String.IsNullOrEmpty(Party))
+                    {
+                        return RedirectToAction("RegisterCandidate", "Candidate", new { @errorID = 3 });
+                    }
+                    if (String.IsNullOrEmpty(Position))
+                    {
+                        return RedirectToAction("RegisterCandidate", "Candidate", new { @errorID = 4 });
+                    }
+                    else
+                    {
+                        Candidate c = new Models.Candidate();
+
+                        c.Candidate_ID = model.Candidate_ID;
+                        c.CandidateFirstNames = model.CandidateFirstNames;
+                        c.CandidateLastName = model.CandidateLastName;
+                        c.CandidatePosition_ID = model.CandidatePosition_ID;
+                        c.PartyID = model.PartyID;
+
+                        try
+                        {
+
+                            if (ModelState.IsValid)
+                            {
+                                db.Candidates.Add(c);
+                                db.SaveChanges();
+                            }
+
+                            TempData["Message"] = "New candidate successfully added!";
+                        }
+                        catch (Exception e)
+                        {
+                            TempData["Message"] = "Add candidate error:" + e;
+                        }
+
+                        return RedirectToAction("StaffHomePage", "Staff");
+                    }
+                case "Cancel":
+                    return RedirectToAction("StaffHomePage", "Staff");
+                default:
+                    return RedirectToAction("StaffHomePage", "Staff");
+            }
+
         }
 
 
-        public ActionResult UpdateOrDeleteCandidate()
+        public ActionResult UpdateOrDeleteCandidate(int? errorID)
         {
+            if (errorID == 1)
+            {
+                ViewBag.Error1 = "Please select a party";
+            }
+            else if (errorID == 2)
+            {
+                ViewBag.Error2 = "Please select a candidate";
+            }
             return View();
         }
- 
- 
-        public ActionResult Choice(string submitbutton)
+
+        public ActionResult Choice(string PartyName, string Candidate, string submitButton)
         {
-            if (submitbutton == "Update")
+            switch (submitButton)
             {
-                return RedirectToAction("UpdateCandidate");
+                case "Update":
+                    //Validation: Prevents candidate from being registered if fields are left blank
+                    if (String.IsNullOrEmpty(PartyName))
+                    {
+                        return RedirectToAction("UpdateOrDeleteCandidate", "Candidate", new { @errorID = 1 });
+                    }
+                    if (String.IsNullOrEmpty(Candidate))
+                    {
+                        return RedirectToAction("UpdateOrDeleteCandidate", "Candidate", new { @errorID = 2 });
+                    }
+                    else
+                    {
+                        //search here 
+                        return RedirectToAction("UpdateCandidate", "Candidate"/*, new {@ID = (id), Id@firstNames = (FirstNames), @lastNames = (LastName), @position = (Position), @party = (Party)*/);
+                    }
+                case "Delete":
+                    return RedirectToAction("Delete", "Candidate");
+                case "Cancel":
+                    return RedirectToAction("StaffHomePage", "Staff");
+                default:
+                    return RedirectToAction("StaffHomePage", "Staff");
             }
-            else if (submitbutton == "Delete")
-            {
-                return RedirectToAction("Delete");
-            }
-            else
-            {
-                return RedirectToAction("StaffHomePage","Staff");
-            }
-            
         }
 
-        public ActionResult UpdateCandidate()
+        public ActionResult UpdateCandidate(string ID, string firstNames, string lastName, string party, string position, int? errorID)
         {
             //viewbags to fill fields with already existing data
-            
+            if (errorID == 1)
+            {
+                ViewBag.Error1 = "Please enter all first names";
+            }
+            else if (errorID == 2)
+            {
+                ViewBag.Error2 = "Please enter a last name";
+            }
+            else if (errorID == 3)
+            {
+                ViewBag.Error3 = "Please select a party";
+            }
+            else if (errorID == 4)
+            {
+                ViewBag.Error4 = "Please select a candidate position";
+            }
             return View();
         }
 
-        public ActionResult Update()
+        public ActionResult Update(string FirstNames, string LastName, string Party, string Position, string submitButton)
         {
-             return RedirectToAction("UpdateOrDeleteCandidate", "Candidate");
+            switch (submitButton)
+            {
+                case "Update":
+                    //Validation: Prevents candidate from being registered if fields are left blank
+                    if (String.IsNullOrEmpty(FirstNames))
+                    {
+                        return RedirectToAction("UpdateCandidate", "Candidate", new { @errorID = 1 });
+                    }
+                    if (String.IsNullOrEmpty(LastName))
+                    {
+                        return RedirectToAction("UpdateCandidate", "Candidate", new { @errorID = 2 });
+                    }
+                    if (String.IsNullOrEmpty(Party))
+                    {
+                        return RedirectToAction("UpdateCandidate", "Candidate", new { @errorID = 3 });
+                    }
+                    if (String.IsNullOrEmpty(Position))
+                    {
+                        return RedirectToAction("UpdateCandidate", "Candidate", new { @errorID = 4 });
+                    }
+                    else
+                    {
+                        return RedirectToAction("UpdateOrDeleteCandidate", "Candidate");
+                    }
+                case "Cancel":
+                    return RedirectToAction("UpdateOrDeleteCandidate", "Candidate");
+                default:
+                    return RedirectToAction("UpdateOrDeleteCandidate", "Candidate");
+            }
         }
 
         public ActionResult Delete()
         {
             return RedirectToAction("UpdateOrDeleteCandidate", "Candidate");
         }
-        
-        public ActionResult TotalVotes()
-        {
-            return View();
-        }
-        
     }
 }
---
+++ INF272Group11Project/ViewModels/CandidateVM.cs
@@ -0,0 +1,26 @@
+﻿using INF272Group11Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272Group11Project.ViewModels
+{
+    public class CandidateVM
+    {
+        //For drop down list for parties
+        public IEnumerable<SelectListItem> Parties { get; set; }
+
+        //For drop down list for candidate positions
+        public IEnumerable<SelectListItem> Positions { get; set; }
+
+        //Accessors and mutators
+        public int Candidate_ID { set; get; }
+        public string CandidateFirstNames { set; get; }
+        public string CandidateLastName { set; get; }
+        public int PartyID { get; set; }
+        public int CandidatePosition_ID { get; set; }
+
+    }
+}
